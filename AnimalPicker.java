07e178a39204ccee44202f34a82f3759992fce8f
dkasy64 <dawitkasy64@gmail.com>
2024-02-25 18:05:33 -0500

    imported scanner and added dog option

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class AnimalPicker{
     public static void main(String[] args){
-        System.out.println("hello world");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What animal would you like to see?");
+        String choice = scan.nextLine();
+        if(choice.equals("dog")){
+            System.out.println("CAT ASCII ART HERE");
+        } 
+
     }
 }
