fa84bf5b7155ca0bbff65b924aea658649aefdd4
dkasy64 <dawitkasy64@gmail.com>
2024-02-25 19:05:06 -0500

    keeps asking user for valid input

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 public class AnimalPicker{
     public static void main(String[] args){
+        boolean option = true;
         Scanner scan = new Scanner(System.in);
-        System.out.println("What animal would you like to see?");
+        System.out.println("What animal would you like to see: a dog or cat?");
+        while(option){
         String choice = scan.nextLine();
         if(choice.equals("dog")){
             System.out.println("DOG ASCII ART HERE");
+            option = false;
         } else if(choice.equals("cat")){
             System.out.println("CAT ASCII ART HERE");
+            option = false;
         } else{
-            System.out.println("Error");
-        }  
+            System.out.println("Enter dog or cat");
+        }
+    }  
     }
 }
